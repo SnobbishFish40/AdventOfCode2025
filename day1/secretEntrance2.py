54d2827575c23c5023df494f9a8295a6c5d7c8e0
Reuben <reubenelliott12345@gmail.com>
2025-12-01 15:13:56 +0000

    completed part 2

--- day1/secretEntrance2.py
+++ day1/secretEntrance2.py
@@ -1,8 +1,10 @@
-# Day 1: Secret Enterance Part 1
+# Day 1: Secret Enterance Part 2
 
 def rotate(rotation, currDial):
     direction = rotation[0]
     distance = int(rotation[1:])
+
+    zeroCounter = 0
 
     if direction == "R":
         distanceToStart = (99 - currDial) + 1
@@ -11,30 +13,38 @@ def rotate(rotation, currDial):
         else:
             distance -= distanceToStart
             currDial = 0
+            zeroCounter += 1
             while distance > 99:
                 distance -= 100
+                zeroCounter += 1
             currDial += distance
-        return currDial
+        if currDial == 0 and distance != 0:
+            zeroCounter += 1
+        return (currDial, zeroCounter)
 
     elif direction == "L":
         if distance <= currDial:
             currDial -= distance
         else:
             distance -= currDial + 1
+            if currDial != 0:
+                zeroCounter += 1
             currDial = 99
             while distance > 99:
                 distance -= 100
+                zeroCounter += 1
             currDial -= distance
-        return currDial
+        if currDial == 0 and distance != 0:
+            zeroCounter += 1
+        return (currDial, zeroCounter)
     else:
-        return currDial
+        return (currDial, zeroCounter)
 
 dial = 50
 password = 0
 with open("input") as puzzleInput:
     for rotation in puzzleInput:
-        dial = rotate(rotation, dial)
-        if dial == 0:
-            password += 1
+        (dial, zeroClicks) = rotate(rotation, dial)
+        password += zeroClicks
 
 print(password)
